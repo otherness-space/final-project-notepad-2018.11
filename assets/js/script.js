0d2657430b297bcc91b9aa2f1732c04c53f6fee6
otherness-space <the@otherness.space>
2018-11-07 16:06:46 -0500

    prelim js array objects

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+let notes = [{
+  noteName: '',
+  noteDateCreate: '',
+  noteDateTimeCreate: '',
+  noteDateModified: '',
+  noteDateTimeModified: '',
+  noteTitle: '',
+  noteBody: '',
+},
+{
+  noteName: '',
+  noteDateCreate: '',
+  noteDateTimeCreate: '',
+  noteDateModified: '',
+  noteDateTimeModified: '',
+  noteTitle: '',
+  noteBody: '',
+}
+];
+
+for (var i = 0; i < notes.length; i++) {
+  document.getElementById('display').innerHTML += ``;
+  // notes[i]
+}
