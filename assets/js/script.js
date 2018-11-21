e6b135459645158551ad3b22e4ffd9784a445cfc
otherness-space <the@otherness.space>
2018-11-21 12:40:55 -0500

    testing changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,57 @@
 let notes = [];
 
-function saveNote(){
+function addNote() {
+  // get values from input
   let noteTitle = document.getElementById('noteTitle').value;
+  let noteBody = document.getElementById('noteBody').value;
+  let noteSnippet = document.getElementById('noteSnippet').value;
 
   notes.push({
-    title: noteTitle
+    title: noteTitle,
+    body: noteBody,
+    snippet: noteSnippet
   });
-
+document.getElementById('noteTitle').value = ``;
   displayNotes();
+  console.log(notes);
 }
 
-function displayNotes(){
+function displayNotes() {
   document.getElementById('displayArchive').innerHTML = '';
-
-  for (let i = 0; i < notes.length; i++) {
-    document.getElementById('display').innerHTML += `<div id="displayArchiveRow" class=" display-archive-row" ondblclick="display-archive-row"></div>`
-    notes[i]
+    for (var i = 0; i < notes.length; i++) {
+      document.getElementById('displayArchive').innerHTML +=
+      `
+      <div class="text-secondary">
+      <i class="material-icons" data-toggle="tooltip" data-html="true" title="open note" onclick="openNote(${i})">note</i>
+      <i class="material-icons" data-toggle="tooltip" data-html="true" title="delete" ondblclick="deleteNote(${i})">clear</i>
+      ${notes[i].title}
+      </div>
+      `;
+      console.log(displayArchive);
+    }
+  // document.getElementById('displayNotePad').innerHTML = '';
+  //   for (var i = 0; i < notes.length; i++) {
+  //     document.getElementById('displayNotePad').innerHTML +=
+  //     `
+  //     ${notes[i].title}
+  //     ${notes[i].body}
+  //     ${notes[i].snippet}
+  //     `;
+  //   }
   }
-}
+
+  function openNote(index) {
+    document.getElementById('title').innerText =
+    `
+    ${notes[index].title}
+    `
+    document.getElementById('noteTitle').innerHTML= `${notes[index].body}`
+    console.log(notes[index].title)
+    displayNotes();
+  }
+
+  function deleteNote(index) {
+    notes.splice(index, 1);
+
+    displayNotes();
+  }
